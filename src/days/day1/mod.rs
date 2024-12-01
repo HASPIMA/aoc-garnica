0a84e3309ec08487f4b7210c85ef1019f1156e2b
SGman98 <bastiangarnica@gmail.com>
2024-12-01 12:29:28 -0500

    feat: add day1 part2 solution

--- src/days/day1/mod.rs
+++ src/days/day1/mod.rs
@@ -20,9 +20,32 @@ pub fn part1(filename: &str) -> Result<i32, &'static str> {
         .zip(right_list.iter())
         .map(|(left, right)| left - right)
         .map(|x| x.abs())
-        .sum::<i32>();
+        .sum();
 
     Ok(distance)
+}
+
+pub fn part2(filename: &str) -> Result<i32, &'static str> {
+    let input_path = format!("data/days/day1/{}", filename);
+    let lines = read_lines(input_path).map_err(|_| "Error reading file")?;
+
+    let mut left_list = vec![];
+    let mut right_list = vec![];
+    for line in lines.map_while(Result::ok) {
+        let mut parts = line.split_whitespace();
+        left_list.push(parts.next().unwrap().parse::<i32>().unwrap());
+        right_list.push(parts.next().unwrap().parse::<i32>().unwrap());
+    }
+
+    let similarity = left_list
+        .iter()
+        .map(|left_item| {
+            let right_count = right_list.iter().filter(|x| *x == left_item).count();
+            right_count as i32 * left_item
+        })
+        .sum();
+
+    Ok(similarity)
 }
 
 #[cfg(test)]
@@ -32,5 +55,11 @@ mod tests {
     fn test_part1() {
         let result = part1("example1.txt").unwrap();
         assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = part2("example1.txt").unwrap();
+        assert_eq!(result, 31);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ fn main() {
     match day.as_str() {
         "1" => {
             days::day1::part1("input1.txt")
-                .map(|x| println!("Res: {}", x))
+                .map(|x| println!("Res part1: {}", x))
+                .unwrap();
+            days::day1::part2("input1.txt")
+                .map(|x| println!("Res part2: {}", x))
                 .unwrap();
         }
         _ => unimplemented!(),
