a19bafb811dd4f52d2bf82044a603f1fd53cb679
SGman98 <bastiangarnica@gmail.com>
2024-12-01 12:41:14 -0500

    feat: improve cli result parsing

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ mod utils;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let day = args.get(1).expect("No day provided");
+    let part = args.get(2).expect("No part provided");
 
-    match day.as_str() {
-        "1" => {
-            days::day1::part1("input1.txt")
-                .map(|x| println!("Res part1: {}", x))
-                .unwrap();
-            days::day1::part2("input1.txt")
-                .map(|x| println!("Res part2: {}", x))
-                .unwrap();
-        }
+    let res = match (day.as_str(), part.as_str()) {
+        ("1", "1") => days::day1::part1("input1.txt"),
+        ("1", "2") => days::day1::part2("input1.txt"),
         _ => unimplemented!(),
+    };
+
+    match res {
+        Ok(result) => println!("Result day {} part {}: {}", day, part, result),
+        Err(err) => eprintln!("Error day {} part {}: {}", day, part, err),
     }
 }
